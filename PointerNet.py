4919dcec1fbb8128eec8ad09fc5c31a431eede7c
shiretzet <me@gurshir.com>
2017-09-13 16:56:21 +0300

    Pointer Network

---
+++ PointerNet.py
@@ -0,0 +1,299 @@
+import torch
+import torch.nn as nn
+from torch.nn import Parameter
+import torch.nn.functional as F
+
+
+class Encoder(nn.Module):
+    """
+    Encoder class for Pointer-Net
+    """
+
+    def __init__(self, embedding_dim,
+                 hidden_dim,
+                 n_layers,
+                 dropout):
+        """
+        Initiate Encoder
+
+        :param Tensor embedding_dim: Number of embbeding channels
+        :param int hidden_dim: Number of hidden units for the LSTM
+        :param int n_layers: Number of layers for LSTMs
+        :param float dropout: Float between 0-1
+        """
+
+        super(Encoder, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
+        self.lstm = nn.LSTM(embedding_dim,
+                            hidden_dim,
+                            n_layers,
+                            dropout=dropout)
+
+        # Used for propagating .cuda() command
+        self.h0 = Parameter(torch.zeros(1), requires_grad=False)
+        self.c0 = Parameter(torch.zeros(1), requires_grad=False)
+
+    def forward(self, embedded_inputs,
+                hidden):
+        """
+        Encoder - Forward-pass
+
+        :param Tensor embedded_inputs: Embedded inputs of Pointer-Net
+        :param Tensor hidden: Initiated hidden units for the LSTMs (h, c)
+        :return: LSTMs outputs and hidden units (h, c)
+        """
+
+        embedded_inputs = embedded_inputs.permute(1, 0, 2)
+
+        outputs, hidden = self.lstm(embedded_inputs, hidden)
+
+        return outputs.permute(1, 0, 2), hidden
+
+    def init_hidden(self, embedded_inputs):
+        """
+        Initiate hidden units
+
+        :param Tensor embedded_inputs: The embedded input of Pointer-NEt
+        :return: Initiated hidden units for the LSTMs (h, c)
+        """
+
+        batch_size = embedded_inputs.size(0)
+
+        # Reshaping (Expanding)
+        h0 = self.h0.unsqueeze(0).unsqueeze(0).repeat(self.n_layers,
+                                                      batch_size,
+                                                      self.hidden_dim)
+        c0 = self.h0.unsqueeze(0).unsqueeze(0).repeat(self.n_layers,
+                                                      batch_size,
+                                                      self.hidden_dim)
+
+        return h0, c0
+
+
+class Attention(nn.Module):
+    """
+    Attention model for Pointer-Net
+    """
+
+    def __init__(self, input_dim,
+                 hidden_dim):
+        """
+        Initiate Attention
+
+        :param int input_dim: Input's diamention
+        :param int hidden_dim: Number of hidden units in the attention
+        """
+
+        super(Attention, self).__init__()
+
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+
+        self.input_linear = nn.Linear(input_dim, hidden_dim)
+        self.context_linear = nn.Conv1d(input_dim, hidden_dim, 1, 1)
+        self.V = Parameter(torch.FloatTensor(hidden_dim), requires_grad=True)
+        self.tanh = nn.Tanh()
+        self.softmax = nn.Softmax()
+
+        # Initialize vector V
+        nn.init.uniform(self.V, -1, 1)
+
+    def forward(self, input,
+                context):
+        """
+        Attention - Forward-pass
+
+        :param Tensor input: Hidden state h
+        :param Tensor context: Attention context
+        :return: tuple of - (Attentioned hidden state, Alphas)
+        """
+
+        # (batch, hidden_dim, seq_len)
+        inp = self.input_linear(input).unsqueeze(2).expand(-1, -1, context.size(1))
+
+        # (batch, hidden_dim, seq_len)
+        context = context.permute(0, 2, 1)
+        ctx = self.context_linear(context)
+
+        # (batch, 1, hidden_dim)
+        V = self.V.unsqueeze(0).expand(context.size(0), -1).unsqueeze(1)
+
+        # (batch, seq_len)
+        att = torch.bmm(V, self.tanh(inp + ctx)).squeeze(1)
+        alpha = self.softmax(att)
+
+        hidden_state = torch.bmm(ctx, alpha.unsqueeze(2)).squeeze(2)
+
+        return hidden_state, alpha
+
+
+class Decoder(nn.Module):
+    """
+    Decoder model for Pointer-Net
+    """
+
+    def __init__(self, embedding_dim,
+                 hidden_dim):
+        """
+        Initiate Decoder
+
+        :param int embedding_dim: Number of embeddings in Pointer-Net
+        :param int hidden_dim: Number of hidden units for the decoder's RNN
+        """
+
+        super(Decoder, self).__init__()
+        self.embedding_dim = embedding_dim
+        self.hidden_dim = hidden_dim
+
+        self.input_to_hidden = nn.Linear(embedding_dim, 4 * hidden_dim)
+        self.hidden_to_hidden = nn.Linear(hidden_dim, 4 * hidden_dim)
+        self.hidden_out = nn.Linear(hidden_dim * 2, hidden_dim)
+        self.att = Attention(hidden_dim, hidden_dim)
+        # Used for propagating .cuda() command
+        self.mask = Parameter(torch.ones(1), requires_grad=False)
+        self.runner = Parameter(torch.zeros(1), requires_grad=False)
+
+    def forward(self, embedded_inputs,
+                decoder_input,
+                hidden,
+                context):
+        """
+        Decoder - Forward-pass
+
+        :param Tensor embedded_inputs: Embedded inputs of Pointer-Net
+        :param Tensor decoder_input: First decoder's input
+        :param Tensor hidden: First decoder's hidden states
+        :param Tensor context: Encoder's outputs
+        :return: (Output probabilities, Pointers indices), last hidden state
+        """
+
+        batch_size = embedded_inputs.size(0)
+        input_length = embedded_inputs.size(1)
+
+        # (batch, seq_len)
+        mask = self.mask.repeat(input_length).unsqueeze(0).expand(batch_size, -1)
+
+        # Generating arang(input_length), broadcasted across batch_size
+        runner = self.runner.repeat(input_length)
+        for i in range(input_length):
+            runner.data[i] = i
+        runner = runner.unsqueeze(0).expand(batch_size, -1).long()
+
+        outputs = []
+        pointers = []
+
+        def recurrence(x, hidden):
+            """
+            Recurrence step function
+
+            :param Tensor x: Input at time t
+            :param tuple(Tensor, Tensor) hidden: Hidden states at time t-1
+            :return: Hidden states at time t (h, c), Attention probabilities (Alpha)
+            """
+
+            # Regular LSTM
+            h, c = hidden
+
+            gates = self.input_to_hidden(x) + self.hidden_to_hidden(h)
+            input, forget, cell, out = gates.chunk(4, 1)
+
+            input = F.sigmoid(input)
+            forget = F.sigmoid(forget)
+            cell = F.tanh(cell)
+            out = F.sigmoid(out)
+
+            c_t = (forget * c) + (input * cell)
+            h_t = out * F.tanh(c_t)
+
+            # Attention section
+            hidden_t, output = self.att(h_t, context)
+            hidden_t = F.tanh(self.hidden_out(torch.cat((hidden_t, h_t), 1)))
+
+            return hidden_t, c_t, output
+
+        # Recurrence loop
+        for _ in range(input_length):
+            h_t, c_t, outs = recurrence(decoder_input, hidden)
+            hidden = (h_t, c_t)
+
+            # Masking selected inputs
+            masked_outs = outs * mask
+
+            # Get maximum probabilities and indices
+            max_probs, indices = masked_outs.max(1)
+            one_hot_pointers = (runner == indices.unsqueeze(1).expand(-1, outs.size()[1])).float()
+
+            # Update mask to ignore seen indices
+            mask  = mask * (1 - one_hot_pointers)
+
+            # Get embedded inputs by max indices
+            embedding_mask = one_hot_pointers.unsqueeze(2).expand(-1, -1, self.embedding_dim).byte()
+            decoder_input = embedded_inputs[embedding_mask.data].view(batch_size, self.embedding_dim)
+
+            outputs.append(outs.unsqueeze(0))
+            pointers.append(indices.unsqueeze(1))
+
+        outputs = torch.cat(outputs).permute(1, 0, 2)
+        pointers = torch.cat(pointers, 1)
+
+        return (outputs, pointers), hidden
+
+class PointerNet(nn.Module):
+    """
+    Pointer-Net
+    """
+
+    def __init__(self, embedding_dim,
+                 hidden_dim,
+                 lstm_layers,
+                 dropout):
+        """
+        Initiate Pointer-Net
+
+        :param int embedding_dim: Number of embbeding channels
+        :param int hidden_dim: Encoders hidden units
+        :param int lstm_layers: Number of layers for LSTMs
+        :param float dropout: Float between 0-1
+        """
+
+        super(PointerNet, self).__init__()
+        self.embedding_dim = embedding_dim
+        self.embedding = nn.Conv1d(2, embedding_dim, 1, 1)
+        self.encoder = Encoder(embedding_dim,
+                               hidden_dim,
+                               lstm_layers,
+                               dropout)
+        self.decoder = Decoder(embedding_dim, hidden_dim)
+        self.decoder_input0 = Parameter(torch.FloatTensor(embedding_dim), requires_grad=False)
+
+        # Initialize decoder_input0
+        nn.init.uniform(self.decoder_input0, -1, 1)
+
+    def forward(self, inputs):
+        """
+        PointerNet - Forward-pass
+
+        :param Tensor inputs: Input sequence
+        :return: Pointers probabilities and indices
+        """
+
+        batch_size = inputs.size(0)
+
+        decoder_input0 = self.decoder_input0.unsqueeze(0).expand(batch_size, -1)
+
+        inputs = inputs.permute(0, 2, 1)
+        embedded_inputs = self.embedding(inputs).permute(0, 2, 1)
+
+        encoder_hidden0 = self.encoder.init_hidden(embedded_inputs)
+        encoder_outputs, encoder_hidden = self.encoder(embedded_inputs,
+                                                       encoder_hidden0)
+
+        decoder_hidden0 = (encoder_hidden[0][-1], encoder_hidden[1][-1])
+
+        (outputs, pointers), decoder_hidden = self.decoder(embedded_inputs,
+                                                           decoder_input0,
+                                                           decoder_hidden0,
+                                                           encoder_outputs)
+
+        return  outputs, pointers
